c6333270d839ecfed39bd2aa863f8bf8bf337fb7
AceGod <acegod@AceGods-Mac.local>
2016-08-17 08:49:47 -0700

    1st commit - intial

--- glen-helen/ViewController.swift
+++ glen-helen/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var geologyBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        geologyBtn.layer.cornerRadius = 2.0
+        
     }
 
     override func didReceiveMemoryWarning() {
